110f75ccdb68e9ca628ba975646dcd574947e9bd
shrikantv5 <64365738+StrixV54@users.noreply.github.com>
2024-01-31 14:40:44 +0530

    Update Graph.js

--- app/components/Graph.js
+++ app/components/Graph.js
@@ -31,22 +31,8 @@ export default function Graph() {
 
   const getData = async (timeframe) => {
     const res = await fetch(
-      `http://localhost:3000/api?timeperiod=${timeframe}`,
-      {
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      }
+      `https://ui-dashboard-full.vercel.app/api?timeperiod=${timeframe}`,
+      { cache: "force-cache" }
     );
     const result = await res.json();
     return result;
