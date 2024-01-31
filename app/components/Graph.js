f70ae306f2c7e08d0c560a5f04e729ca174441f8
shrikantv5 <64365738+StrixV54@users.noreply.github.com>
2024-01-31 14:39:01 +0530

    Update Graph.js

--- app/components/Graph.js
+++ app/components/Graph.js
@@ -32,7 +32,21 @@ export default function Graph() {
   const getData = async (timeframe) => {
     const res = await fetch(
       `http://localhost:3000/api?timeperiod=${timeframe}`,
-      { cache: "force-cache" }
+      {
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      }
     );
     const result = await res.json();
     return result;
